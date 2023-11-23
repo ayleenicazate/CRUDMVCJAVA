b1c952197c4e5e2a38dc55f026a4f5b4446e1b99
ayleenicazate <ayl.guajardo@duocuc.cl>
2023-11-23 15:13:47 -0300

    Listas y Buscar por nombre hecho

--- src/cl/mingoalmacen/vista/JF_CarritoCompras.java
+++ src/cl/mingoalmacen/vista/JF_CarritoCompras.java
@@ -4,7 +4,13 @@
  */
 package cl.mingoalmacen.vista;
 
+import DAO.ProductoDAO;
+import cl.mingoalmacen.controller.ControladorProducto;
 import cl.mingoalmacen.model.Producto;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -28,6 +34,18 @@ public class JF_CarritoCompras extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jtbl_productos = new javax.swing.JTable();
+        jbtn_listarProd = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jtxt_nombreBusc = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jtxt_precioBusc = new javax.swing.JTextField();
+        jbtn_nombreBusc1 = new javax.swing.JButton();
+        jbtn_precioBusc1 = new javax.swing.JButton();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jmnu_carritoCompras = new javax.swing.JMenu();
@@ -38,6 +56,128 @@ public class JF_CarritoCompras extends javax.swing.JFrame {
         jMenu2 = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(255, 153, 153));
+        jPanel1.setForeground(new java.awt.Color(255, 255, 255));
+
+        jtbl_productos.setBackground(new java.awt.Color(255, 204, 204));
+        jtbl_productos.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 0, 0), 1, true));
+        jtbl_productos.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "ID", "Nombre", "Precio", "Stock Disponible"
+            }
+        ));
+        jScrollPane1.setViewportView(jtbl_productos);
+
+        jbtn_listarProd.setText("Listar Productos");
+        jbtn_listarProd.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbtn_listarProdActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI Black", 0, 14)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setText("Productos disponibles:");
+
+        jtxt_nombreBusc.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jtxt_nombreBuscActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setFont(new java.awt.Font("Segoe UI Black", 1, 18)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setText("Buscar productos por:");
+
+        jLabel3.setFont(new java.awt.Font("Segoe UI Black", 1, 14)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel3.setText("Nombre:");
+
+        jLabel4.setFont(new java.awt.Font("Segoe UI Black", 1, 14)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel4.setText("Precio menor a: ");
+
+        jtxt_precioBusc.setBackground(new java.awt.Color(242, 242, 242));
+        jtxt_precioBusc.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jtxt_precioBuscActionPerformed(evt);
+            }
+        });
+
+        jbtn_nombreBusc1.setText("Buscar");
+        jbtn_nombreBusc1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbtn_nombreBusc1ActionPerformed(evt);
+            }
+        });
+
+        jbtn_precioBusc1.setText("Buscar");
+        jbtn_precioBusc1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbtn_precioBusc1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 494, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jbtn_listarProd)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 506, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel2)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 117, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(jLabel3))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addComponent(jtxt_nombreBusc, javax.swing.GroupLayout.PREFERRED_SIZE, 114, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addGap(18, 18, 18)
+                                        .addComponent(jbtn_nombreBusc1))
+                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addComponent(jtxt_precioBusc, javax.swing.GroupLayout.PREFERRED_SIZE, 114, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addGap(18, 18, 18)
+                                        .addComponent(jbtn_precioBusc1)))))))
+                .addContainerGap(61, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(21, 21, 21)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel3)
+                            .addComponent(jbtn_nombreBusc1)
+                            .addComponent(jtxt_nombreBusc, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel4)
+                            .addComponent(jbtn_precioBusc1)
+                            .addComponent(jtxt_precioBusc, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 567, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jbtn_listarProd)
+                .addGap(36, 36, 36))
+        );
 
         jMenu1.setText("Opciones");
 
@@ -79,24 +219,85 @@ public class JF_CarritoCompras extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 800, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 677, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jmi_comprarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmi_comprarActionPerformed
-    // TODO add your handling code here:
+        // TODO add your handling code here:
 
     }//GEN-LAST:event_jmi_comprarActionPerformed
 
     private void jmi_mostrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmi_mostrarActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jmi_mostrarActionPerformed
+
+    private void jbtn_listarProdActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtn_listarProdActionPerformed
+        int idProd;
+        String nombreProd;
+        int precio;
+        int stock;
+
+        ControladorProducto cp = new ControladorProducto();
+        DefaultTableModel modelo = (DefaultTableModel) this.jtbl_productos.getModel();
+
+        modelo.setRowCount(0);
+
+        List<Producto> lista = cp.buscarTodos();
+
+        for (Producto producto : lista) {
+            idProd = producto.getId_prod();
+            nombreProd = producto.getNombre_prod();
+            precio = producto.getPrecio();
+            stock = producto.getStock();
+
+            modelo.addRow(new Object[]{idProd, nombreProd, precio, stock});
+        }
+    }//GEN-LAST:event_jbtn_listarProdActionPerformed
+
+    private void jtxt_nombreBuscActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jtxt_nombreBuscActionPerformed
+
+    }//GEN-LAST:event_jtxt_nombreBuscActionPerformed
+
+    private void jtxt_precioBuscActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jtxt_precioBuscActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jtxt_precioBuscActionPerformed
+
+    private void jbtn_nombreBusc1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtn_nombreBusc1ActionPerformed
+
+        String nombreProd = this.jtxt_nombreBusc.getText(); // Obtener el nombre del campo de texto
+        int idProd;
+        int precio;
+        int stock;
+
+        ControladorProducto cp = new ControladorProducto();
+        DefaultTableModel modelo = (DefaultTableModel) this.jtbl_productos.getModel();
+
+        modelo.setRowCount(0);
+
+        List<Producto> prodBusc = cp.buscarPorNombre(nombreProd);
+
+        for (Producto producto : prodBusc) {
+            idProd = producto.getId_prod();
+            nombreProd = producto.getNombre_prod();
+            precio = producto.getPrecio();
+            stock = producto.getStock();
+
+            modelo.addRow(new Object[]{idProd, nombreProd, precio, stock});
+        }
+
+
+    }//GEN-LAST:event_jbtn_nombreBusc1ActionPerformed
+
+    private void jbtn_precioBusc1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtn_precioBusc1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jbtn_precioBusc1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -112,16 +313,24 @@ public class JF_CarritoCompras extends javax.swing.JFrame {
                 if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
+
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(JF_CarritoCompras.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(JF_CarritoCompras.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(JF_CarritoCompras.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(JF_CarritoCompras.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(JF_CarritoCompras.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(JF_CarritoCompras.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(JF_CarritoCompras.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(JF_CarritoCompras.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
@@ -134,13 +343,25 @@ public class JF_CarritoCompras extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JButton jbtn_listarProd;
+    private javax.swing.JButton jbtn_nombreBusc1;
+    private javax.swing.JButton jbtn_precioBusc1;
     private javax.swing.JMenuItem jmi_actualizar;
     private javax.swing.JMenuItem jmi_comprar;
     private javax.swing.JMenuItem jmi_eliminar;
     private javax.swing.JMenuItem jmi_mostrar;
     private javax.swing.JMenu jmnu_carritoCompras;
+    private javax.swing.JTable jtbl_productos;
+    private javax.swing.JTextField jtxt_nombreBusc;
+    private javax.swing.JTextField jtxt_precioBusc;
     // End of variables declaration//GEN-END:variables
 }
